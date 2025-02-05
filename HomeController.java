933746a152562667978fba721a651a57defe1511
dhivya1806 <92577481+dhivya1806@users.noreply.github.com>
2025-02-05 15:39:33 +0000

    First commit

---
+++ HomeController.java
@@ -0,0 +1,25 @@
+package com.example.samplewebapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}package com.example.samplewebapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
